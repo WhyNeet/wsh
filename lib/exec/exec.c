a4c4e5bcddb2208e36b19fbf0bd20278aa7c25dc
git push force <117515380+WhyNeet@users.noreply.github.com>
2025-08-08 13:15:19 +0300

    feat: implement ls

--- lib/exec/exec.c
+++ lib/exec/exec.c
@@ -1,15 +1,33 @@
 #include "exec.h"
+#include <dirent.h>
 #include <stdio.h>
+#include <unistd.h>
+
+void exec_echo(Command *command) {
+  for (int i = 1; i < command->argc; i++) {
+    printf("%s ", command->argv[i]);
+  }
+  printf("\n");
+}
+
+void exec_ls(Command *command) {
+  char cwd[PATH_MAX];
+  getcwd(cwd, sizeof(cwd));
+  DIR *d = opendir(cwd);
+  struct dirent *dir;
+
+  while ((dir = readdir(d)) != NULL) {
+    printf("%s\n", dir->d_name);
+  }
+}
 
 void exec_command(Command *command) {
   switch (command->type) {
   case CMD_ECHO:
-    for (int i = 1; i < command->argc; i++) {
-      printf("%s ", command->argv[i]);
-    }
-    printf("\n");
+    exec_echo(command);
     break;
   case CMD_LS:
+    exec_ls(command);
     break;
   case CMD_EXTERNAL:
     break;
